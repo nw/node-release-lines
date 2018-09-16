338b6f4fe23e373c798714820a1b332318ff8ea9
Nathan White <nw@nwhite.net>
2018-09-16 14:25:36 -0700

    add tests to test group api and basic filtering on release api

---
+++ test/group.unit.js
@@ -0,0 +1,52 @@
+/* global describe it */
+const { NodeReleaseGroup } = require('../')
+const assert = require('assert')
+const data = require('./fixtures/release.json')
+const controlDate = new Date('2018-09-01')
+
+describe('NodeReleaseGroup', function () {
+  it('load data via static `load`', function () {
+    let releases = NodeReleaseGroup.load(data)
+    assert(releases.length, 10, 'length of releases is incorrect')
+  })
+
+  it('respect date', function () {
+    let releases = NodeReleaseGroup.load(data, controlDate)
+    assert.strictEqual(releases.getSupported().length, 3, 'supported releases')
+    assert.strictEqual(releases.getCurrent().length, 1, 'current releases')
+    assert.strictEqual(releases.getMaintenance().length, 1, 'maintenance releases')
+    assert.strictEqual(releases.getFuture().length, 1, 'future releases')
+    assert.strictEqual(releases.getActive().length, 1, 'active releases')
+    assert.strictEqual(releases.getEOL().length, 6, 'EOL releases')
+    assert.strictEqual(releases.getModern().length, 8, 'modern releases')
+    assert.strictEqual(releases.getLTS().length, 4, 'LTS releases')
+  })
+
+  it('should have 2 current releases', function () {
+    let releases = NodeReleaseGroup.load(data, new Date('2017-10-15').getTime())
+    assert.strictEqual(releases.getCurrent().length, 2, 'current releases')
+  })
+
+  it('should have 2 active lts releases', function () {
+    let releases = NodeReleaseGroup.load(data, new Date('2018-02-28').getTime())
+    assert.strictEqual(releases.getActive().length, 2, 'active releases')
+  })
+
+  it('should have 4 supported releases', function () {
+    let releases = NodeReleaseGroup.load(data, new Date('2018-06-17').getTime())
+    assert.strictEqual(releases.getSupported().length, 4, 'supported releases')
+  })
+
+  it('should support subquery filters on releases', function () {
+    let releases = NodeReleaseGroup.load(data, new Date('2018-10-31').getTime())
+
+    assert.strictEqual(releases.getActive().length, 2, 'active releases')
+    assert.strictEqual(releases.getSupported().length, 4, 'supported releases')
+    assert.strictEqual(releases.getSupported().getLTS().length, 3, 'supported LTS releases')
+    // redundant
+    assert.strictEqual(releases.getSupported().getLTS().getActive().length, 2, 'LTS releases')
+
+    assert.strictEqual(releases.getEOL().length, 6, 'EOL releases')
+    assert.strictEqual(releases.getModern().getEOL().length, 4, 'Modern EOL releases')
+  })
+})
