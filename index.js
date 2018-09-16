b7716fc898ade16f04c882ea6cdd56f9effb9e86
Nathan White <nw@nwhite.net>
2018-09-16 14:11:48 -0700

    first pass at api

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const NodeReleaseGroup = require('./lib/NodeReleaseGroup')
+const NodeRelease = require('./lib/NodeRelease')
+
+module.exports = {
+  NodeReleaseGroup: NodeReleaseGroup,
+  NodeRelease: NodeRelease
+}
