c57e1852551789f94103d98e6d9e6fa810c21270
Nathan White <nw@nwhite.net>
2018-09-18 00:44:29 -0700

    added Vulnerability class. added doc, download helpers for Release

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 
 const ReleaseLines = require('./lib/ReleaseLines')
 const ReleaseLine = require('./lib/ReleaseLine')
+const Releases = require('./lib/Releases')
 const Release = require('./lib/Release')
+const Vulnerability = require('./lib/Vulnerability')
 
-module.exports = {
-  ReleaseLines: ReleaseLines,
-  ReleaseLine: ReleaseLine,
-  Release: Release
-}
+module.exports = { ReleaseLines, ReleaseLine, Releases, Release, Vulnerability }
