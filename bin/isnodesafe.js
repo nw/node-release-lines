6944cdd31eec9eb908b7f6de77502da390a67279
Tierney Cyren <hello@bnb.im>
2018-09-29 21:29:33 -0400

    Remove usage of `action` (version passing is broken)

--- bin/isnodesafe.js
+++ bin/isnodesafe.js
@@ -15,42 +15,37 @@ handleInvalid(release)
 cli
 .version('1.0.0', '-v, --version')
 .usage('[options]')
-.description('CLI tool to check if Node.js is safe. Defaults to checking system version.')
+.description("CLI tool to check if Node.js is safe. Defaults to checking system version.")
 .option('-c, --ci', 'Returns a non-zero exit code if the version of Node.js is not safe, and a zero exit code if it is safe.')
 .option('-r, --release [release]', 'Checks to see if a specific release of Node.js is safe')
-.action(function() {
-  if (release.isSafe) {
-    if (!cli.ci) {
-      console.log('')
-      console.log('✅  Node.js ' + version + ' is safe!')
-      console.log('')
-    }
-    process.exit(0)
-  } else {
-    if (cli.release) {
-      if(!cli.release.indexOf("v")) {
-        version = cli.release
-      } else {
-        version = "v" + cli.release
-      }
-    }
-    if (!cli.ci) {
-      var releaseLine = version.split('.')[0]
-      var safeReleases = api.Releases.load(releaseLine).getSafe()
-        var safe = safeReleases[safeReleases.length - 1]
-  
-        handleInvalid(safe)
-  
-        console.log('')
-        console.log('⚠️  Node.js ' + version + ' is not safe! You should upgrade now.')
-        console.log('')
-        console.log('👉  Minimum safe Node.js version in the ' + releaseLine + ' release line: ' + safe.version)
-        console.log('')
-      }
-      process.exit(1)
-    }
-  })
 .parse(process.argv)
+
+if (release.isSafe) {
+  if (!cli.ci) {
+    console.log('')
+    console.log('✅  Node.js ' + version + ' is safe!')
+    console.log('')
+  }
+  process.exit(0)
+} else {
+  if (cli.release) {
+    version = "v" + cli.release
+  }
+  if (!cli.ci) {
+    var releaseLine = version.split('.')[0]
+    var safeReleases = api.Releases.load(releaseLine).getSafe()
+      var safe = safeReleases[safeReleases.length - 1]
+
+      handleInvalid(safe)
+
+      console.log('')
+      console.log('⚠️  Node.js ' + version + ' is not safe! You should upgrade now.')
+      console.log('')
+      console.log('👉  Minimum safe Node.js version in the ' + releaseLine + ' release line: ' + safe.version)
+      console.log('')
+    }
+    process.exit(1)
+  }
 
 
 function handleInvalid (release) {
